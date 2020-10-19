a16fc9a7ce9e00275b721595c323b7e16553ca51
fred <ricodoby@hotmail.com>
2020-10-19 22:03:25 +0200

    lire la derniere ligne

--- check_ip.sh
+++ check_ip.sh
@@ -27,6 +27,7 @@
 # Parameters
 LOGPATH="/var/log/checkip"
 LOGFILE="/var/log/checkip/checkip.log"
+IPFILE="/var/log/checkip/ipAddress.log"
 OVH_DNS=""
 IPTOCHECKWEBACCESS="8.8.8.8"
 GETPUBLICADDRESSE="https://ipinfo.io/ip"
@@ -55,13 +56,13 @@ check_file_exist(){
 	if [ ! $? = 0 ]; then
 		write_log "ERROR unable to create file: $1" $LOGPATH $LOGFILE
 	else
-		write_log "INFO file creatde: $1" $LOGPATH $LOGFILE
+		write_log "INFO file created: $1" $LOGPATH $LOGFILE
 	fi
 }
 
 # write logfile msg $1 path $2 pathfile $3
 write_log(){
-	check_file_exist $2
+	check_folder_exist $2
 	check_file_exist $3
 	echo dateTime_generator" $1" >> $3
 }
@@ -74,10 +75,10 @@ check_web_access() {
 	ping -c1 $1
 	if [ ! $? = 0]; then
 		write_log "ERROR cant reach webip: $1" $LOGPATH $LOGFILE
-		return 0
+		return 1
 	else
 		write_log "INFO webip $1 reached" $LOGPATH $LOGFILE
-		return 1
+		return 0
 	fi
 }
 
@@ -85,11 +86,26 @@ get_my_ip(){
 	MYIP=`curl "$1"`
 	if [ ! $? = 0]; then
 		write_log "ERROR cant get public ip from : $1" $LOGPATH $LOGFILE
-		return 0
+		return 1
 	else
 		write_log "INFO public address: $MYIP" $LOGPATH $LOGFILE
-		return 1
+		return 0
 	fi
+}
+
+exit_program() {
+	write_log "ERROR: $1" $LOGPATH $LOGFILE
+	exit 1
+}
+
+read_last_line_from_log(){
+	return (tail -n 1 $1)
+}
+
+compare_last_publicIp_and_actual_publicIp() {
+	LASTIPFROMFILE= read_last_line_from_log $IPFILE
+	echo $LASTIPFROMFILE
+	# if [ $LASTIPFROMFILE = $MYIP]
 }
 
 send_email(){
@@ -97,3 +113,13 @@ send_email(){
 }
 
 # Logic
+# Check internet access
+if [ check_web_access = 1 ]; then
+	exit_program "No Internet access"
+
+if [ get_my_ip = 0 ]; then
+	write_log $MYIP $LOGPATH $IPFILE
+else
+	exit_program "Public_IP KO"	
+
+compare_last_publicIp_and_actual_publicIp
